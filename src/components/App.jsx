30408284381d63ab7e50293c091bd77b50a866ad
EmmzyTech <emmzytech@gmail.com>
2022-12-16 17:38:07 -0500

    Committed some card components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,27 @@ import './styles.css';
 import React from "react";
 import Card from "./Card";
 import contacts from '../contacts';
+import Avatar from './Avatar';
+
+function createCard(contact){
+  return (
+    <Card 
+    key={contact.id}
+    name={contact.name}
+    imgURL={contact.imgURL}
+    phone={contact.phone}
+    email={contact.email}
+    />
+    )
+}
 
 function App(){
   return <div>
   <h1>Card Details</h1>
-    <Card 
-    name={contacts[0].name}
-    imgURL={contacts[0].imgURL}
-    phone={contacts[0].phone}
-    email={contacts[0].email}
-    />
-    <Card 
-    name={contacts[1].name}
-    imgURL={contacts[1].imgURL}
-    phone={contacts[1].phone}
-    email={contacts[1].email}
-    />
-    <Card 
-    name={contacts[2].name}
-    imgURL={contacts[2].imgURL}
-    phone={contacts[2].phone}
-    email={contacts[2].email}
-    />
-   
-    </div>
+  <Avatar img="https://media.licdn.com/dms/image/D4D35AQGP6yKTKKEhXQ/profile-framedphoto-shrink_400_400/0/1666379215143?e=1671804000&v=beta&t=2NsMYdhDOiWej-pD5ePDr9rF7BOaSRcRw-LT62rs5ZE"/>
+  {contacts.map(createCard)}
+
+  </div>
 }
 
 export default App;
